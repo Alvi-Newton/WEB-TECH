3961ff6c94c8527d4778ad7806b46dd59ba84d84
Alvi-Newton <101501398+Alvi-Newton@users.noreply.github.com>
2022-08-08 13:54:57 +0600

    Add files via upload

--- login.php
+++ login.php
@@ -1,33 +1,61 @@
 <?php
+	session_start();
+
+	if(isset($_SESSION['id'])){
+		header("LOCATION: ../view/WelcomePage.php");
+	} 
 ?>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Form Submission</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login Form</title>
+    <link rel="stylesheet" href="../view/css/login.css" type="text/css">
 </head>
 <body>
-		<form action="loginCheck.php" method="post" enctype="">
-			<fieldset>
-				<legend>LOGIN</legend>
-				<table>
-					<tr>
-						<td>User Id</td>
-						<td><input type="text" name="User ID" value=""></td>
-					</tr>
-					<tr>
-						<td>password</td>
-						<td><input type="password" name="password" value=""></td>
-					</tr>
+<form name="form2" id="form2" action="../controller/loginAction.php" method="post" novalidate>
+	<fieldset>
+		<legend><h2>Login Form</h2></legend>
 
-                    <tr>
-	                <td>
-	                    <input type="button" name="Login" value="Login">
-                        <a href="registration.php" >Register</a>
-	                </td>
-	            </tr>
+		<table>
 
-				
-				</table>
-			</fieldset>
-		</form>
+			<tr height="40px">
+
+                    <td width="100px">
+                        <label for="uName">User Name*:</label>
+                    </td>
+                    <td>
+                        <input type="text" name="uName" id="uName" value="<?php if(isset($_COOKIE["uName"])) { echo $_COOKIE["uName"]; } ?>">
+                    </td>
+            </tr>
+
+            <tr height="40px">
+
+                    <td width="100px">
+                        <label for="pass">Password*:</label>
+                    </td>
+                    <td>
+                        <input type="text" name="pass" id="pass" value="<?php if(isset($_COOKIE["pass"])) { echo $_COOKIE["pass"]; } ?>">
+                    </td>
+            </tr>
+        </table>
+
+        <input type="checkbox" name="rememberMe" id="rememberMe"><label>Remember me</label>
+
+        <br><br>
+
+		<!-- <a href="../view/ForgottenPassword.php">Forgotten Password?</a> -->
+
+		<br><br>
+
+		<input type="submit" name="login">
+
+		<br><br>
+
+		<label>Don't have an account?</label> <a href="../view/registresion.html">Sign-up</a>
+	</fieldset>
+
+</form>
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
-<?php
+<?php 
+	session_start();
+	session_unset();
+	session_destroy();
 
-session_start();
-session_destroy();
-header('location: registration.php');
+	header("LOCATION: ../view/login.php");
 ?>
